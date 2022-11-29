0bef5ef99cb9e863ff230dc9f048f4cdb2303b4e
Jace <jkang86@my.bcit.ca>
2022-11-28 18:53:54 -0800

    Add player sprite

---
+++ components/player.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Spaceship(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Spaceship, self).__init__()
+        self.image = pygame.image.load("./images/player.png")
+        self.rect = self.image.get_rect()
+        self.centerx = self.rect.centerx
+        self.centery = self.rect.centery
+
+    def set_position(self, x, y):
+        self.rect.x = x - self.centerx
+        self.rect.y = y - self.centery
+
+    def collide(self, sprites):
+        for sprite in sprites:
+            if pygame.sprite.collide_rect(self, sprite):
+                return sprite
+
