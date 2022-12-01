c715603d616a248f4d94fe003d8f4f25f012983d
Jace <jkang86@my.bcit.ca>
2022-11-30 17:50:30 -0800

    Rename main file, add state

---
+++ dodge_alien.py
@@ -0,0 +1,36 @@
+import pygame
+from screens import WelcomeScreen, GameScreen, GameOverScreen, UsernameScreen
+
+class Game:
+
+    def __init__(self):
+
+        self.window = pygame.display.set_mode((800, 600))
+
+    def run(self):
+        screens = {
+            "welcome": WelcomeScreen,
+            "username": UsernameScreen,
+            "game": GameScreen,
+            "game_over": GameOverScreen,
+        }
+
+        running = True
+        current_screen = "welcome"
+        state = {}
+        while running:
+            screen_class = screens.get(current_screen)
+            screen = screen_class(self.window, state)
+
+            screen.run()
+
+            if screen.next_screen is False:
+                running = False
+
+            current_screen = screen.next_screen
+            state = screen.state
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
